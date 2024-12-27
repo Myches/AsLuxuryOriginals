5bb51bde6893cd375b26019f969520e0cad3da80
Michael Andorful <andorfulmichaelebo@gmail.com>
2024-12-27 18:17:42 +0000

    Fix the navbar submenus

--- asluxuryoriginals/src/components/Navbar.tsx
+++ asluxuryoriginals/src/components/Navbar.tsx
@@ -51,9 +51,9 @@ export default function Navbar() {
     <nav className="w-full h-auto fixed font-montserrat border-b border-gray-200 bg-white z-50 pt-3">
       <div className="flex flex-col space-y-5">
         {/* Top Bar */}
-        <div className="relative flex items-center p-4 md:px-16">
+        <div className="relative flex items-center p-4 lg:px-16">
           <div
-            className="md:hidden text-2xl cursor-pointer"
+            className="lg:hidden text-2xl cursor-pointer"
             onClick={() => setMenuOpen(!menuOpen)}
           >
             {menuOpen ? <HiX /> : <HiOutlineMenu />}
@@ -63,12 +63,12 @@ export default function Navbar() {
             to="/"
             className="absolute left-1/2 transform -translate-x-1/2 whitespace-nowrap"
           >
-            <p className="text-xl md:text-4xl font-bold text-black inline-block">
+            <p className="text-xl lg:text-4xl font-bold text-black inline-block">
               AS LUXURY ORIGINALS
             </p>
           </Link>
 
-          <div className="hidden md:flex lg:space-x-4 lg:text-2xl space-x-2 text-md ml-auto">
+          <div className="hidden lg:flex space-x-4 text-2xl ml-auto">
             <button
               onClick={() => setIsModalOpen(true)}
               className="hover:text-gray-400"
@@ -87,7 +87,7 @@ export default function Navbar() {
         {/* Desktop Navigation */}
         <div className="flex justify-around items-center w-full">
           <div
-            className="hidden md:flex justify-center space-x-6 py-4 relative"
+            className="hidden lg:flex justify-center space-x-6 py-4 relative"
             onMouseLeave={handleMouseLeave}
           >
             {links.map((link) => (
@@ -108,7 +108,7 @@ export default function Navbar() {
                 </Link>
                 {activeMenu === link && sublistContent[link] && (
                   <div
-                    className="fixed left-0 right-0 bg-white border-t shadow-lg z-20 mt-4"
+                    className="fixed left-0 right-0 bg-white border-t shadow-lg z-20 mt-4 hidden lg:block"
                     onMouseEnter={handleDropdownMouseEnter}
                     onMouseLeave={handleDropdownMouseLeave}
                   >
@@ -144,17 +144,17 @@ export default function Navbar() {
             ))}
           </div>
 
-          <div className="hidden md:flex">
+          <div className="hidden lg:flex">
             <SearchInput onItemSelect={handleSearchItemSelect} />
           </div>
-          <div className="hidden md:flex p-3 text-md border rounded-lg">
+          <div className="hidden lg:flex p-3 text-md border rounded-lg">
             <SearchFilter onItemSelect={handleSearchItemSelect} />
           </div>
         </div>
 
         {/* Mobile Menu */}
         <div
-          className={`${menuOpen ? "block" : "hidden"} md:hidden px-8 p-6`}
+          className={`${menuOpen ? "block" : "hidden"} lg:hidden px-8 p-6`}
         >
           <div className="flex flex-col space-y-4">
             {links.map((link) => (
@@ -170,27 +170,6 @@ export default function Navbar() {
                 >
                   {link}
                 </Link>
-                {sublistContent[link] && (
-                  <div className="ml-4 mt-2 space-y-2">
-                    {sublistContent[link].map((subcategory, index) => {
-                      const categoryPath = getCategoryPath(link, subcategory);
-                      return (
-                        <Link
-                          key={index}
-                          to={categoryPath}
-                          className={`block text-sm ${
-                            isActiveLink(categoryPath)
-                              ? "text-black font-medium"
-                              : "text-gray-500"
-                          }`}
-                          onClick={() => setMenuOpen(false)}
-                        >
-                          {subcategory}
-                        </Link>
-                      );
-                    })}
-                  </div>
-                )}
               </div>
             ))}
             <div className="flex justify-center space-x-6 mt-6 text-2xl">
@@ -208,10 +187,10 @@ export default function Navbar() {
               </button>
             </div>
 
-            <div className="block md:hidden my-3">
+            <div className="block lg:hidden my-3">
               <SearchInput onItemSelect={handleSearchItemSelect} />
             </div>
-            <div className="block md:hidden p-3 text-md border rounded-lg">
+            <div className="block lg:hidden p-3 text-md border rounded-lg">
               <SearchFilter onItemSelect={handleSearchItemSelect} />
             </div>
           </div>
